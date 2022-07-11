5b1007ffd0475b688547f55db71a8b6b73365aeb
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2022-07-11 18:10:40 +0700

    rest api awal

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+const express = require('express')
+const PORT = 3000
+
+const app = express()
+
+//CREATE API
+//method GET
+//endpoint: http://<ipaddress>:3000/hello-world
+app.get('/hello-world',(req, res) => {
+    res.json({hi: 'Hello World'})
+})
+
+app.listen(PORT, "0.0.0.0", ()=>{
+    console.log(`Connected to port ${PORT} haloo`)
+})
